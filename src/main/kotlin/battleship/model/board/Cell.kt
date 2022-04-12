563cdb0f34b5b5b6033ad7f558d8a655de3097d8
rodrigohcorreia <rodrigo.h.correia@gmail.com>
2022-04-12 18:42:05 +0100

    Cell bue weird

--- src/main/kotlin/battleship/model/board/Cell.kt
+++ src/main/kotlin/battleship/model/board/Cell.kt
@@ -1,17 +1,20 @@
 package battleship.model.board
 
 import battleship.model.Position.Position
+import battleship.model.Position.indexToColumn
 import battleship.model.Position.indexToRow
 import battleship.model.Ship.ShipType
-import indexToColumn
+import battleship.ui.BOARD_CHAR_DIM
 
-open class Cell {
-    open val pos: Position = Position((-1).indexToColumn(), (-1).indexToRow()) // in this case class showed be open // throw Illegal POS
-    open val content: CellContent = CellContent.WATER
+open class Cell private constructor(val content: CellContent = CellContent.WATER){
+    open val pos: Position = Position((-1).indexToColumn(), (-1).indexToRow()) // ?start at -1 to throw Illegal POS
 
-    open fun toChar(): Char = content.toChar()
+    companion object {
+        val values: List <Position> = listOf(BOARD_CHAR_DIM){ Position(/*?? é privado*/) }
+        open fun toChar(): Char = content.toChar()
+    }
+
 }
-
 enum class CellContent(private val id: Char) {
     WATER(' '),
     SHIP('#'),
@@ -22,9 +25,7 @@ enum class CellContent(private val id: Char) {
         return this.id
     }
 }
-
 class ShipCell(val pos: Position, val ship: ShipType) : Cell()
-
 enum class Direction(val dir: Char) {
     HORIZONTAL('H'), VERTICAL('V');
 
@@ -32,5 +33,4 @@ enum class Direction(val dir: Char) {
         return this.dir.toString()
     }
 }
-
 class Ship(val pos: Position, val type: ShipType, val dir: Direction) : ShipCell(pos, type)
